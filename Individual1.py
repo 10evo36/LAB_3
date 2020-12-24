2c5dec71f3dd579461b053388785b77a08eb98d8
10evo36 <75244122+10evo36@users.noreply.github.com>
2020-12-24 15:19:10 +0300

    Update Individual1.py

--- Individual1.py
+++ Individual1.py
@@ -3,7 +3,7 @@
 n = int(input("Сколько вам лет? "))
 if n <= 0:
     print("Вы ввели некорректное число!")
-elif n > 1 and n < 5 or n == 22 or n == 33 or n == 44:
+elif n %100 > 1 and n %100 < 5 or n == 22 or n == 33 or n == 44:
     print("Мне", n, "года")
 elif n == 1:
     print("Мне", n, "год")
